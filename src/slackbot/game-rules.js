ac9f3357168c053b0e695398b3abd2a73f61ee93
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-02-18 18:08:29 +0100

    game rules helpers that checks status of the game

---
+++ src/slackbot/game-rules.js
@@ -0,0 +1,47 @@
+const possibleResponses = require('../../config/options');
+
+module.exports = {
+  drawByAllPossibilites(gameCounter) {
+    return gameCounter.every((item) => {
+      return item > 0;
+    });
+  },
+
+  drawByOnlyOnePossibility(gameCounter) {
+    var optionsCount = 0;
+    gameCounter.forEach((item) => {
+      if (item) optionsCount++
+    });
+    return optionsCount === 1;
+  },
+
+  giveWinners(gameCounter, users) {
+    var winningOption = null;
+    if (gameCounter[0] && gameCounter[1]) {
+      winningOption = 'scissors';
+    } else if (gameCounter[1] && gameCounter[2]) {
+      winningOption = 'paper';
+    } else {
+      winningOption = 'rock';
+    }
+    const winners = users.filter((item) => {
+      return item.response === winningOption;
+    });
+
+    return winners;
+  },
+
+  processToGameCounter(results) {
+    const gameCounter = [];
+
+    possibleResponses.forEach((item) => {
+      gameCounter.push(0);
+    });
+
+    results.forEach((result) => {
+      gameCounter[possibleResponses.indexOf(result.response)]++;
+    });
+
+    return gameCounter;
+  }
+};
