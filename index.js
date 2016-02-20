d675241ba653714ecb3103ab9bf9903891f80c30
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-02-20 09:29:16 +0100

    add http - create very simple server to wake up heroku dyno

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const RtmClient = require('slack-client').RtmClient;
 const WebClient = require('slack-client').WebClient;
 const RTM_EVENTS = require('slack-client').RTM_EVENTS;
@@ -20,6 +21,7 @@ rtm.start();
 // Setup users hash
 require('./src/initializers/users-hash-initializer')(web);
 
+// basic slackBot loop
 rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, () => {
   rtm.on(RTM_EVENTS.MESSAGE, (message) => {
     if (!message.text) return;
@@ -31,3 +33,10 @@ rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, () => {
     }
   });
 });
+
+// basic http server
+http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write('Good morning!');
+  response.end();
+}).listen(process.env.PORT);
