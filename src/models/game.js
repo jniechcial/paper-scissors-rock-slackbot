f4f21bde535d54eafa22759c07b79602fceb370c
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-02-25 22:09:00 +0100

    add calc game-winners task

--- src/models/game.js
+++ src/models/game.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const GameSchema = Schema({
   playerIds: [String],
+  winners: [String],
   channelId: {
     type: String
   }
---
+++ src/tasks/games-winners.js
@@ -0,0 +1,25 @@
+// Database setup
+require('../../config/mongo-db')();
+
+// Models
+const Game = require('../models/game');
+const GameResponse = require('../models/game-response');
+
+// Game Rules
+const gameRules = require('../slackbot/game-rules');
+
+Game.find({}).lean().exec((err, results) => {
+  results.forEach((game) => {
+    GameResponse.find({ 'gameId': game._id }).lean().exec((err, results) => {
+      if (game.playerIds.length !== results.length) return;
+
+      const gameCounter = gameRules.processToGameCounter(results);
+      const drawByAll = gameRules.dhrawByAllPossibilites(gameCounter);
+      const drawByOne = gameRules.drawByOnlyOnePossibility(gameCounter);
+      if (!drawByAll && !drawByOne) {
+        const winners = gameRules.giveWinners(gameCounter, results).map(winner => winner.userId);
+        Game.update({ _id: game._id }, { winners });
+      }
+    });
+  })
+});
